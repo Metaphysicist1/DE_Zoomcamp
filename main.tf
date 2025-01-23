c82d3cca01aaf7d97c56bb752ac428e5c145875c
Metaphysicist1 <edgarabasov1@gmail.com>
2025-01-23 20:12:00 +0400

    init terraform

---
+++ main.tf
@@ -0,0 +1,30 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "6.17.0"
+    }
+  }
+}
+
+
+provider "google" {
+  project = "data-experiments-448712"
+  region  = "us-central1"
+  
+}
+
+resource "google_storage_bucket" "experimental_terraform_bucket" {
+  name          = "engineering_terraform_bucket"
+  location      = "US"
+  force_destroy = true
+
+  lifecycle_rule {
+    condition {
+      age = 1
+    }
+    action {
+      type = "Delete"
+    }
+  }
+}
